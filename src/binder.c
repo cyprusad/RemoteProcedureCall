4a65a4dd2f679852e8fef258deb87649064cbbd5
Sai Warang <cyprusad@gmail.com>
2014-07-15 01:26:38 -0400

    Removed some duplicate includes

--- src/binder.c
+++ src/binder.c
@@ -10,11 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/wait.h>
-#include <netinet/in.h>
-#include <netdb.h>
+
 
 #include "rpc.h"
 #include "socket_stream.h" 
