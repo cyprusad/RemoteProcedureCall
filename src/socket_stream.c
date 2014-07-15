3466bf3fd29eb2db9fddc66da95f118925a9902b
Sai Warang <cyprusad@gmail.com>
2014-07-15 01:51:21 -0400

    remove header

--- src/socket_stream.c
+++ src/socket_stream.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 
 #define MAXHOSTNAME 1023
