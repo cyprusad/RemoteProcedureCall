c75bce814fa04840187dfb7886d7fdb9e0402c9a
Sai Warang <cyprusad@gmail.com>
2014-07-18 12:53:23 -0400

    Missed this in prev commit

---
+++ src/response_codes.h
@@ -0,0 +1,20 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+ 
+#define BD_NEW_FUNC_REGISTERED     100
+#define BD_DUPL_FUNC_FOR_SERVER    101
+#define BD_NEW_SERVER_FOR_FUNC     102
+
+#define BS_SUCC_SEND_CLIENT        140
+#define BS_SUCC_SEND_SERVER        141
+#define BS_SUCC_SEND_TERM_SERVER   142
+
+#define B_S_KEEP_CONN_ALIVE        170 
+
+#define INVALID_MESSAGE            666
+
+
+#ifdef __cplusplus
+}
+#endif
